cdaac59e1e2ff8be33b97f27e1db5276e22b02d8
gabriel geoffrey henderso <gabe1980@LinuxLab07.byui.edu>
2017-07-13 19:41:30 -0600

    core1

--- course.cpp
+++ course.cpp
@@ -8,16 +8,21 @@ using namespace std;
 Course::Course(int size)
 {
    // TODO: Fill this in
+   this -> size = size;
+   
+   classList = new Student [this -> size];
 }
 
 Student Course::getStudent(int index) const
 {
    // TODO: Fill this in
+   return classList[index];
 }
 
 void Course::setStudent(int index, const Student & student)
 {
    // TODO: Fill this in
+   classList[index] = student;
 }
 
 void Course::displayList() const
